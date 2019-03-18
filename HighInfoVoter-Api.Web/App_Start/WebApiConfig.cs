9ccca112fc5ab03c8a09e470df322c0135e796e6
Jared Henderson <jared.p.henderson@gmail.com>
2019-03-18 13:32:30 -0700

    Added Cors.

--- HighInfoVoter-Api.Web/App_Start/WebApiConfig.cs
+++ HighInfoVoter-Api.Web/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace HighInfoVoter_Api.Web
 {
@@ -10,6 +11,8 @@ namespace HighInfoVoter_Api.Web
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            var cors = new EnableCorsAttribute("www.example.com", "*", "*");
+            config.EnableCors(cors);
 
             // Web API routes
             config.MapHttpAttributeRoutes();
